5fff10bc82b71fda6467f5d1b509074cc2bcd228
laranapoli <laranapoli0@gmail.com>
2024-10-26 18:55:57 -0300

    add verbose flag

--- fast_deploy/cli.py
+++ fast_deploy/cli.py
@@ -1,22 +1,40 @@
 import subprocess
 
 from rich.console import Console
-from typer import Argument, Typer
+from typer import Argument, Option, Typer, prompt
 
 console = Console()
 app = Typer()
 
 
 @app.command()
-def serve():
+def serve(
+    image_name: str = Argument('fastdeploy', help='Nome da imagem Docker'),
+    port: int = Argument(8000, help='Porta para expor a API'),
+    verbose: bool = Option(
+        False, '--verbose', help='Flag para exibir logs do container'
+    ),
+):
+
     import os
 
     console.print('Buildando Docker container...')
-    build_command = ['docker', 'build', '-t', 'fastdeploy', '.']
-    subprocess.run(build_command, check=True)
+    build_command = ['docker', 'build', '-t', image_name, '.']
+    if verbose:
+        subprocess.run(build_command, check=True)
+    else:
+        subprocess.run(
+            build_command,
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
-    console.print('Startando Docker container')
-    run_command = ['docker', 'run', '-p', '8000:8000', 'fastdeploy']
+    console.print('Startando Docker container.')
+    if verbose:
+        run_command = ['docker', 'run', '-p', f'{port}:8000', image_name]
+    else:
+        run_command = ['docker', 'run', '-d', '-p', f'{port}:8000', image_name]
     subprocess.run(run_command, check=True)
 
 
