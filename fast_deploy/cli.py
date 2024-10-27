68ddec267488d5afc6df00d8aae48266d95f05db
laranapoli <laranapoli0@gmail.com>
2024-10-27 19:29:05 -0300

    feat: adds docker installation check and update command name

--- fast_deploy/cli.py
+++ fast_deploy/cli.py
@@ -7,17 +7,30 @@ console = Console()
 app = Typer()
 
 
+def check_docker_installed():
+    try:
+        subprocess.run(
+            ['docker', '--version'],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    except FileNotFoundError:
+        console.print(
+            '[bold red]Docker não está instalado. Por favor, instale o Docker para usar esta aplicação.'
+        )
+        raise SystemExit(1)
+
+
 @app.command()
-def serve(
+def deploy(
     image_name: str = Argument('fastdeploy', help='Nome da imagem Docker'),
     port: int = Argument(8000, help='Porta para expor a API'),
     verbose: bool = Option(
         False, '--verbose', help='Flag para exibir logs do container'
     ),
 ):
-
-    import os
-
+    check_docker_installed()
     console.print('Buildando Docker container...')
     build_command = ['docker', 'build', '-t', image_name, '.']
     if verbose:
